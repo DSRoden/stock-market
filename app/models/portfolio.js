3622433c487e11bff9e7111fd5f37f8daeda5c78
Daniel S. Roden <daniel.s.roden@gmail.com>
2014-07-17 12:44:57 -0500

    adding stocks to portfolio

--- app/models/portfolio.js
+++ app/models/portfolio.js
@@ -1,6 +1,35 @@
+'use strict';
+
+var Stock = require('./stock');
+
 function Portfolio(name) {
   this.name = name;
   this.stocks = [];
+}
+
+Portfolio.prototype.add = function(symbol, amount) {
+  var stock = findStock(this.stocks, symbol);
+
+  if(stock) {
+    stock.count += amount;
+  } else {
+    stock = new Stock(symbol, amount);
+    this.stocks.push(stock);
+  }
+
+  console.log(stock);
+};
+
+//Private Helper Functions //
+
+function findStock(stocks, symbol){
+  for(var i = 0; i < stocks.length; i++) {
+    if(stocks[i].symbol === symbol.toUpperCase()) {
+      return stocks[i];
+   }
+  }
+
+  return null;
 }
 
 
--- test/unit/portfolio.js
+++ test/unit/portfolio.js
@@ -4,6 +4,7 @@
 'use strict';
 
 var expect = require('chai').expect;
+var Stock = require('../../app/models/stock');
 var Portfolio = require('../../app/models/portfolio');
 
 describe('Portfolio', function() {
@@ -14,6 +15,22 @@ describe('Portfolio', function() {
       expect(port1).to.be.instanceof(Portfolio);
       expect(port1.name).to.equal('port1');
       expect(port1.stocks.length).to.equal(0);
+    });
+  });
+
+  describe('#add', function () {
+    it('should add stocks to portfolio', function() {
+      var port1 = new Portfolio ('port1');
+      port1.add('aapl', 50);
+      port1.add('aapl', 25);
+      port1.add('amzn', 30);
+      
+      expect(port1.stocks).to.have.length(2);
+      expect(port1.stocks[0]).to.be.instanceof(Stock);
+      expect(port1.stocks[0].count).to.equal(75);
+      expect(port1.stocks[1].count).to.equal(30);
+      
+      
     });
   });
 });
